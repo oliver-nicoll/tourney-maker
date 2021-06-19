aa1adb932ef1102c9644f7cc934f0271b57a322c
Nicoll Oliver <nicolldon96@gmail.com>
2021-06-19 17:47:25 -0400

    navbar start

--- src/Components/Pages/Pages.js
+++ src/Components/Pages/Pages.js
@@ -1,18 +1,44 @@
 import React, { useState } from 'react';
-import About from ''
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
+import Tournaments from './Components/Tournaments/Tournaments';
+import Teams from './Components/Teams/Teams';
+import ScoreBoard from './Components/ScoreBoard/ScoreBoard'
 
 const Pages = () => {
+    const [page, setPage] = useState('about')
+
+    const renderPage = () => {
+        switch (page) {
+            case "about":
+                
+                return <About />;
+            case "tournaments":
+            
+                return <Tournaments />;
+            case "teams":
+            
+                return <Teams />;
+            case "scoreboard":
+            
+                return <ScoreBoard />;
+        
+            default:
+                return <Home />;
+        }
+    }
 
     return (
         <div>
             <nav>
-                <button>About</button>
-                <button>Tournaments</button>
-                <button>Teams</button>
-                <button>Score Board</button>
-
-
+                <button onClick={() => setPage("about")}>About</button>
+                <button onClick={() => setPage("tournaments")}>Tournaments</button>
+                <button onClick={() => setPage("teams")}>Teams</button>
+                <button onClick={() => setPage("scoreboard")}>Score Board</button>
             </nav>
+            <div>
+                {renderPage()}
+            </div>
         </div>
     )
 }
