7f9dc78bff45c8295166826c8ad9d8dbceb0582a
Nicoll Oliver <nicolldon96@gmail.com>
2021-06-22 15:59:05 -0400

    app.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,49 @@
 import './App.css';
-import { useSelector } from 'react-redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Pages from './Components/Pages/Pages';
+import { useSelector, useDispatch } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+// import { actionCreators } from './State/index';
+import { Route, Switch } from 'react-router-dom';
 import NavBar from './Components/NavBar/NavBar'
 import Home from './Components/home/Home';
-import TeamForm from './Components/teams/TeamForm';
+import Teams from './Components/teams/Teams';
 import ScoreBoard from './Components/scoreboard/ScoreBoard';
 import Tournaments from './Components/tournaments/Tournaments';
 import About from './Components/about/About'
+//import {decrement, increment} from "./Redux/counter"
 
 function App() {
 
-  const state = useSelector((state) => state.keyTestReducer)
+  const state = useSelector((state) => state.counter);
 
+  const dispatch = useDispatch()
+
+  // const { addTeam, removeTeam, addEvent, removeEvent } = bindActionCreators(actionCreators, dispatch)
+
+  
   console.log(state)
 
   return (
-    <div className="App">
-       <Router>
-            <div>
-              <Route exact path="/about" component={ About } />
-              <Route exact path="/teams" component={ TeamForm } />
-              <Route exact path="/tournaments" component={ Tournaments } />
-              <Route exact path="/scorekeeper" component={ ScoreBoard } />
-            </div>
-          </Router>
-        <NavBar />
-      <Home />
-      <Pages />
+    <div className="App" >
+      <NavBar />
+      <div className="app__image">
+      <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/teams">
+            <Teams />
+          </Route>
+          <Route path="/events">
+            <Tournaments />
+          </Route>
+          <Route path="/scoreboard">
+            <ScoreBoard />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+       </div>
     </div>
   );
 }
