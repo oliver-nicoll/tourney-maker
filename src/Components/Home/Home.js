b2fa678524bd46f19329077da5dc0245a604c307
Nicoll Oliver <nicolldon96@gmail.com>
2021-06-22 16:00:44 -0400

    home js page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import "../home/Home.css"
+import "../home/Home.css";
+
 
 const Home = () => {
     return (
         <div className="home">
             <div className="home__container">
-                <img className="home__image" src="https://images.unsplash.com/photo-1536566482680-fca31930a0bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=934&q=80" alt="https://images.unsplash.com/photo-1583591900414-7031eb309cb6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=975&q=80" />
+                <img className="home__image" src="https://images.unsplash.com/photo-1536566482680-fca31930a0bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=934&q=80" alt=""/>
                 Home Page: Tourney-Maker
+            </div>
+
+            <div className="home__row" >
+               
+            </div>
+
+            <div className="home__row" >
+                
+            </div>
+
+            <div className="home__row" >
+               
             </div>
         </div>
     );
