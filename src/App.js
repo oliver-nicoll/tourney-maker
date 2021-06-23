bae3ce980658457623993d0665901b3dfc4d98be
Nicoll Oliver <nicolldon96@gmail.com>
2021-06-23 13:38:38 -0400

    axios install and remove dispatch and selector hooks from app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import './App.css';
-import { useSelector, useDispatch } from 'react-redux';
-// import { bindActionCreators } from 'redux';
-// import { actionCreators } from './State/index';
 import { Route, Switch } from 'react-router-dom';
+
 import NavBar from './Components/NavBar/NavBar'
 import Home from './Components/home/Home';
 import Teams from './Components/teams/Teams';
 import ScoreBoard from './Components/scoreboard/ScoreBoard';
 import Tournaments from './Components/tournaments/Tournaments';
 import About from './Components/about/About'
-//import {decrement, increment} from "./Redux/counter"
 
 function App() {
 
-  const state = useSelector((state) => state.counter);
-
-  const dispatch = useDispatch()
-
-  // const { addTeam, removeTeam, addEvent, removeEvent } = bindActionCreators(actionCreators, dispatch)
-
-  
-  console.log(state)
 
   return (
     <div className="App" >
