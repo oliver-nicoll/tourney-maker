59c07957f4b6952aea58c8cea0c030b5dbd93588
Nicoll Oliver <nicolldon96@gmail.com>
2021-06-25 13:34:31 -0400

    tournaments

--- src/Components/Tournaments/Tournaments.js
+++ src/Components/Tournaments/Tournaments.js
@@ -2,26 +2,31 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { fetchTournaments, addTournament } from '../../Redux/tournamentSlice';
-
+import Teams from '../teams/Teams'
 import "../tournaments/Tournaments.css"
 
 const Tournaments = () => {
     const dispatch = useDispatch()
+    
+    const [newEventName, setNewEventName] = useState("")
+    const [newDate, setNewDate] = useState("")
+    const [newDesc, setNewDesc] = useState("")
+    const [newHost, setNewHost] = useState("")
+
 
     const tournaments = useSelector((state) => {
         return state.tournaments.all
     })
     
     useEffect(() => {
-        // console.log("mounting tournaments")
+        console.log("mounting tournaments")
         dispatch(fetchTournaments())
 
-        // return () => {
-        //     console.log("unmounting tournaments")
-        // }
+        return () => {
+            console.log("unmounting tournaments")
+        }
     }, [])
 
-    const [newEventName, setNewEventName] = useState("")
 
     let handleSubmit = (e) => {
         e.preventDefault()
@@ -39,6 +44,25 @@ const Tournaments = () => {
         setNewEventName(e.target.value)
     }
 
+    const handleDateChange = (e) => {
+        setNewDate(e.target.value)
+    }
+
+    const handleDescChange = (e) => {
+        setNewDesc(e.target.value)
+    }
+
+    const handleHostChange = (e) => {
+        setNewHost(e.target.value)
+    }
+
+    // const handleTeamChange = (e) => {
+    //     setNewEventTeam(e.target.value)
+    // }
+    // const handleOnChange = () => {
+    //     setIsChecked(!isChecked);
+    //   };
+
 
     return (
         <div className="main">
@@ -47,11 +71,14 @@ const Tournaments = () => {
                 {tournaments.map((t, i) => <li key={i}>{t.tourney_name}</li>)}
             </ul>
             <form onSubmit={handleSubmit}>
-                <input type="text" value={newEventName} onChange={handleChange} />
+                Tournament Name: <input type="text" value={newEventName} onChange={handleChange} />
+                Tournament Date: <input type="text" value={newDate} onChange={handleDateChange} />
+                Tournament Description: <input type="text" value={newDesc} onChange={handleDescChange} />                
+                Tournament Host: <input type="text" value={newHost} onChange={handleHostChange} />                
+                <br></br>
                 <input type="submit" value="Add New Tournament" />
             </form>
         </div>
     )
 }
-
 export default Tournaments
