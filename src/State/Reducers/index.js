a991679182eca8bf1d7857fe39178931b58afb9b
Nicoll Oliver <nicolldon96@gmail.com>
2021-06-24 21:31:57 -0400

    delete un needed files

--- src/State/Reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import AddTeam from "./AddTeam";
-import AddEvent from "./AddEvent";
-
-const reducers = combineReducers({
-    keyAddTeam: AddTeam,
-    keyAddEvent: AddEvent});
-
-export default reducers;
