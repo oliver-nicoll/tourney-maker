f829aa91409728e1977fe439073cdb8b7d911561
Nicoll Oliver <nicolldon96@gmail.com>
2021-06-25 13:34:19 -0400

    new join

---
+++ src/Redux/registrationSlice.js
@@ -0,0 +1,35 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const getById = createAsyncThunk('registration/getById', async (tournament_id) => {
+    const resp = await fetch( `http://localhost:3000//api/v1/tournaments/${tournament_id}/registrations`).fetchById(tournament_id)
+    const data = await resp.json()
+    console.log(data)
+})
+
+const registrationSlice = createSlice({
+    name: 'registration',
+    initialState: { 
+        tournaments: [],
+        teams: [],
+        loading: false,
+    },
+    reducers: {
+        startLoading: (state) => {
+           return state.loading = true
+        },
+
+        endLoading: (state) => {
+           return state.loading = false
+        }
+    },
+    extraReducers: (builder) => {
+        builder
+        .addCase(getById.fulfilled, ((state, action) => {
+            state.tournaments.push(action.payload)
+        }))
+    },
+})
+
+export const { startLoading, endLoading } = registrationSlice.actions
+
+export default registrationSlice.reducer
