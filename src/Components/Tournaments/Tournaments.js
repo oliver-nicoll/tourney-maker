c30e1559f3523162289bd42ee0627dce2c7e083a
Nicoll Oliver <nicolldon96@gmail.com>
2021-06-26 08:39:07 -0400

    seperating code and organizing

--- src/Components/Tournaments/Tournaments.js
+++ src/Components/Tournaments/Tournaments.js
@@ -1,83 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-
-import { fetchTournaments, addTournament } from '../../Redux/tournamentSlice';
-import Teams from '../teams/Teams'
+import React  from 'react';
+import { useSelector } from 'react-redux';
+import {Link} from "react-router-dom";
+import TournamentForm from '../tournaments/TournamentForm'
 import "../tournaments/Tournaments.css"
 
 const Tournaments = () => {
-    const dispatch = useDispatch()
-    
-    const [newEventName, setNewEventName] = useState("")
-    const [newDate, setNewDate] = useState("")
-    const [newDesc, setNewDesc] = useState("")
-    const [newHost, setNewHost] = useState("")
-
 
     const tournaments = useSelector((state) => {
         return state.tournaments.all
     })
-    
-    useEffect(() => {
-        console.log("mounting tournaments")
-        dispatch(fetchTournaments())
-
-        return () => {
-            console.log("unmounting tournaments")
-        }
-    }, [])
-
-
-    let handleSubmit = (e) => {
-        e.preventDefault()
-        if(newEventName) {
-            dispatch(addTournament({
-                tourney_name: newEventName
-            }))
-            setNewEventName("")
-        } else {
-            alert("must fill out box")
-        }
-    }
-    
-    const handleChange = (e) => {
-        setNewEventName(e.target.value)
-    }
-
-    const handleDateChange = (e) => {
-        setNewDate(e.target.value)
-    }
-
-    const handleDescChange = (e) => {
-        setNewDesc(e.target.value)
-    }
-
-    const handleHostChange = (e) => {
-        setNewHost(e.target.value)
-    }
-
-    // const handleTeamChange = (e) => {
-    //     setNewEventTeam(e.target.value)
-    // }
-    // const handleOnChange = () => {
-    //     setIsChecked(!isChecked);
-    //   };
 
 
     return (
         <div className="main">
             <h3>Events:</h3>
             <ul>
-                {tournaments.map((t, i) => <li key={i}>{t.tourney_name}</li>)}
+                {tournaments.map((t, i) => <li className="event__list" key={i}><Link to={`/events/${t.id}`}>{t.tourney_name}</Link>
+                </li>)}
             </ul>
-            <form onSubmit={handleSubmit}>
-                Tournament Name: <input type="text" value={newEventName} onChange={handleChange} />
-                Tournament Date: <input type="text" value={newDate} onChange={handleDateChange} />
-                Tournament Description: <input type="text" value={newDesc} onChange={handleDescChange} />                
-                Tournament Host: <input type="text" value={newHost} onChange={handleHostChange} />                
-                <br></br>
-                <input type="submit" value="Add New Tournament" />
-            </form>
+            <TournamentForm />
         </div>
     )
 }
