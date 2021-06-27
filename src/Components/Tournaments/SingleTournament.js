3bf3dde9e45cca0732150c9c23b0c3b132d7c936
Nicoll Oliver <nicolldon96@gmail.com>
2021-06-27 17:03:38 -0400

    fetch works without redux fetch

--- src/Components/Tournaments/SingleTournament.js
+++ src/Components/Tournaments/SingleTournament.js
@@ -2,34 +2,61 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from "react-router-dom";
 import { addTeamToTournament } from '../../Redux/tournamentSlice';
-//import Team from '../teams/Teams'
-//import { getById } from '../../Redux/registrationSlice';
+import { getById } from '../../Redux/registrationSlice';
+import {selectTeamById} from '../../Redux/teamsSlice';
+import DisplayTeamTournaments from '../teams/DisplayTeamTournaments'
 
-const SingleTournament = ( ) => {
+
+const SingleTournament = ({match}) => {
+    //const { teamId } = match.params
 //useSelector to find the tournament with that id from the store and render on page
 //add team to tournament here - form 
    const dispatch = useDispatch()
 
     const { id } = useParams()
 
+    //const team = useSelector(state => selectTeamById(state, teamId))
+
     const tournament = useSelector((state) => state.tournaments.all.find((tournament) => tournament.id == id))
 //just to grab info from store
     const teams = useSelector((state) => state.teams.all)
+
    
+    
     const [teamPick, setTeamPick] = useState("")
 
+
     const handleOnChange = (e) => {
-        setTeamPick(e.target.value)
-       
+        setTeamPick(e.target.value)  
+        console.log(teamPick)
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        dispatch(addTeamToTournament(tournament.id, teamPick))
-
         console.log(teamPick)
+        createRegistration()
+       // dispatch(addTeamToTournament(tournament.id, teamPick))
         setTeamPick("")
     }
+
+    const createRegistration = () => {
+        fetch( `http://localhost:3000//api/v1/tournaments/${id}/registrations`,  {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                    tournament_id: id,
+                    team_id: teamPick
+                }
+            )
+        })
+    }
+
+
+    
+
 
     return(
         <div>
@@ -39,19 +66,24 @@ const SingleTournament = ( ) => {
                     {tournament.tourney_name }
                 </div>
                 <div className="tournament__info">
-                Date: {tournament.date}<br/><br/>Host:{tournament.host}<br/><br/>
+                    Date: {tournament.date}<br/><br/>Host:{tournament.host}<br/><br/>
                     Description: {tournament.description}
                 </div>
                 <div>
-                <br/><br/>
-                <br/><br/>
-                <form onSubmit={handleSubmit}>
-                    <input list="add__team" value={teamPick} onChange={handleOnChange} />
-                    <datalist id="add__team">
-                        {teams.map((t) => <option key={t.id} value={t.id}>{t.team_name}</option> )}
-                    </datalist>
-                    <input type="submit" value="Add Team"/>
-                </form>
+                    <DisplayTeamTournaments />
+                </div>
+
+                <div className="form__container__team">
+                    <br/><br/>
+                    <br/><br/>
+                        <form onSubmit={handleSubmit}>
+                            <input list="add__team" value={teamPick} onChange={handleOnChange} />
+                                <datalist id="add__team">
+                                        {teams.map((t, i) => 
+                                            <option key={i} value={t.id}>{t.team_name}</option>)} 
+                                </datalist>
+                            <input type="submit" value="Add Team"/>
+                        </form>    
                 </div>
             </div>
             : "Tournament not Found"}
