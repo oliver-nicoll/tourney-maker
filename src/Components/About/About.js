dee94892c601b8188b0b3852e71a14dc63e10555
Nicoll Oliver <nicolldon96@gmail.com>
2021-06-28 08:33:11 -0400

    fixing complications

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import picture from '../about/1.png'
+
 import "../about/About.css"
 
 function About() {
     return (
         <div className="about__container">
+            <div>
+                <img className="about__image" src={picture} alt=""/>
+            </div>
+            
             About
         </div>
     );
