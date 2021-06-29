525257e9071aa6e761617c4689b2766210f20a84
Nicoll Oliver <nicolldon96@gmail.com>
2021-06-29 08:46:49 -0400

    changes to import rts

--- src/Components/ScoreBoard/ScoreBoard.js
+++ src/Components/ScoreBoard/ScoreBoard.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { decrement, increment, selectCount } from '../../Redux/counter';
+import Counter from './counter.js'
+// import picture from './score.png'
 import "../scoreboard/ScoreBoard.css"
 
 const ScoreBoard = () => {
 
-    const count = useSelector(selectCount)
-
-    const dispatch = useDispatch()
+    
 
     return (
         <>
-            <div className="score-board">
-                <span> Team Score: {count} </span><br/>
-                <div>
-                    <button onClick={() => dispatch(increment())} data-add="1"> + 1</button>
-                    <button onClick={() => dispatch(decrement())} data-subtract="1"> - 1</button>
-                </div><br></br>
+            <div className="scoreboard__main">
+                <div className="picture__scoreboard">
+                    <div className="counter__scoreboard">
+                        <Counter />
+                    </div>
+                    {/* <img src={picture} /> */}
+                </div>
             </div>
         </>
     )
