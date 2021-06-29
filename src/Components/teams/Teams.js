b2a83ccf85915b433782f9a27c0067a1a4437c03
Nicoll Oliver <nicolldon96@gmail.com>
2021-06-29 08:47:54 -0400

    import cleanup

--- src/Components/teams/Teams.js
+++ src/Components/teams/Teams.js
@@ -3,14 +3,10 @@ import { useSelector } from 'react-redux';
 import TeamForm from '../teams/TeamForm'
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-
 import "../teams/Teams.css"
-import Volleyball from '../teams/Volleyball.png'
 
 const useStyles = makeStyles({
     root: {
@@ -29,12 +25,11 @@ const useStyles = makeStyles({
     },
   });
 
+  
+
 const Teams = () => {
 
-
     const classes = useStyles();
-   
-    const bull = <span className={classes.bullet}>•</span>;
    
 
     const teams = useSelector((state) => {
@@ -50,20 +45,21 @@ const Teams = () => {
             </div><br></br>
             <div className="team__card">
                 <h2>Team List:</h2>
-            {teams.map((t, i) =>
-                <Card className={classes.root}>
-                    <CardContent className="card__content">
-                        <Typography className={classes.id} color="textSecondary" gutterBottom key={i}> Volleyball Team Name:  <br /></Typography>
-                        <Typography variant="h5" component="h2">{t.team_name}
-                        </Typography><br></br>
-                        <Typography className={classes.id} color="textSecondary">
-                        Team Captain:
-                        </Typography>
-                        <Typography variant="body2" component="p">
-                        {t.team_captain}
-                        </Typography>
-                    </CardContent>
-                </Card>)}
+            {teams.map((t, i) => 
+                  <Card className={classes.root}>
+                      <CardContent className="card__content">
+                          <Typography className={classes.id} color="textSecondary" gutterBottom key={i}> Volleyball Team Name:  <br /></Typography>
+                          <Typography variant="h5" component="h2">{t.team_name}
+                          </Typography><br></br>
+                          <Typography className={classes.id} color="textSecondary">
+                          Team Captain:
+                          </Typography>
+                          <Typography variant="body2" component="p">
+                          {t.team_captain}
+                          </Typography>
+                      </CardContent>
+                  </Card>
+              )}
             </div>
         </div>
     )
