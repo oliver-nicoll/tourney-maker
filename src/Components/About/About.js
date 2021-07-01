a781b2d0e9dda2368f342ae82e0bc6da3d79eb23
Nicoll Oliver <nicolldon96@gmail.com>
2021-07-01 10:45:14 -0400

    about static fixed

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -6,11 +6,12 @@ import "../about/About.css"
 function About() {
     return (
         <div className="about__container">
-            <div>
-                <img className="about__image" src={picture} alt=""/>
-            </div>
-            
-            About
+            <h1>About Us:</h1>
+            <h4>This is an educational app that allows you to create 
+                and view your tournaments/teams.<br></br> Here at Tourney Maker we strive at keeping the functionality 
+                growing and imporving.<br></br> Stay tuned for future updates with the bracket maker
+                for the volleyball event you create.
+            </h4>
         </div>
     );
 }
