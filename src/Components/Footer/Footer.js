65a8091c71bc85e934755f5eb638659d84b87d49
Nicoll Oliver <nicolldon96@gmail.com>
2021-07-03 09:36:39 -0400

    create footer js

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  align-items: center;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;  
+  background-color: black;
+  @media (max-width: 1000px) {
+    padding: 70px 30px;
+  }
+`;
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    max-width: 1000px;
+    margin: 0 auto;
+`
+
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  margin-left: 60px;
+`;
+
+export const Row = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
+  grid-gap: 20px;
+  @media (max-width: 1000px) {
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  }
+`;
+
+export const Link = styled.a`
+  color: #fff;
+  margin-bottom: 5px;
+  font-size: 12px;
+  text-decoration: none;
+  &:hover {
+      color: #ff9c00;
+      transition: 200ms ease-in;
+  }
+`;
+
+export const Title = styled.p`
+  font-size: 18px;
+  color: #fff;
+  margin-bottom: 0px;
+  font-weight: bold;
+`;
