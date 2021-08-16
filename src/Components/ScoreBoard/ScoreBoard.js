6af6b7d0b94f31ec6d05fca1ddeb13c677ec9648
Nicoll Oliver <nicolldon96@gmail.com>
2021-08-15 23:57:05 -0400

    fixes and changes to scoreboard component

--- src/Components/ScoreBoard/ScoreBoard.js
+++ src/Components/ScoreBoard/ScoreBoard.js
@@ -1,23 +1,73 @@
 import React from 'react';
-import Counter from './counter.js'
-// import picture from './score.png'
-import "../scoreboard/ScoreBoard.css"
+import Header from './Header.js'
+import AddTeamForm from './AddTeamForm.js';
+import TeamSB from './TeamSB.js';
 
-const ScoreBoard = () => {
 
+class ScoreBoard extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            teamSB: [
+                {
+                    name: 'Team A',
+                    score: 0,
+                    game: 1
+                },
+                {
+                    name: 'Team B',
+                    score: 0,
+                    game: 1
+                }
+            ]
+        };
+        this.onScoreChange = this.onScoreChange.bind(this)
+        this.onTeamAdd = this.onTeamAdd.bind(this)
+        // this.onTeamRemove = this.onTeamRemove.bind(this)
+    }
+            
     
-
-    return (
-        <>
-            <div className="scoreboard__main">
-                <div className="picture__scoreboard">
-                    <div className="counter__scoreboard">
-                        <Counter />
-                    </div>
+    onScoreChange = (index, delta) => {
+        this.state.teamSB[index].score += delta;
+        this.setState(this.state);
+    }
+    
+    onTeamAdd(name) {
+        this.state.teamSB.push({name: name, score: 0, game: 1})
+        this.setState(this.state)
+    }
+    
+    onTeamRemove(e) {
+        const target = e.target.value;
+        console.log(target)
+        // this.state.teamSB.splice(index, 1)
+        // this.setState(this.state)
+    }
+    
+    render() {
+        return (
+                <div className="scoreboard__main">
+                   <Header teamSB={this.state.teamSB}/>
+                   <div className="teams">
+                       {this.state.teamSB.map(function (team, index) {
+                           return (
+                               <TeamSB 
+                                name={team.name}
+                                score={team.score}
+                                game={team.game}
+                                key={team.name}
+                                onScoreChange={(delta) => this.onScoreChange(index, delta)}
+                                onRemove={() => this.onTeamRemove}
+                                />
+                           );
+                        }.bind(this)
+                       )}
+                   </div>
+                   <AddTeamForm onAdd={this.onTeamAdd} />
                 </div>
-            </div>
-        </>
-    )
-} 
+        )
+    }
+}
 
 export default ScoreBoard
