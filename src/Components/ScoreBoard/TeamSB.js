607ab34ff14c5519bb4e441f0365fa20189765a5
Nicoll Oliver <nicolldon96@gmail.com>
2021-08-16 07:47:18 -0400

    Remove feature works

--- src/Components/ScoreBoard/TeamSB.js
+++ src/Components/ScoreBoard/TeamSB.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import Counter from './counter'
 
-function TeamSB(props) {
-
-    return (
-        <div className="team">
-            <div className="team__name">
-                <button className="remove__team" onClick={(e) => props.onTeamRemove}>x</button>
-                {props.name}
+class TeamSB extends React.Component {
+   
+    render() {
+        return (
+            <div className="team">
+                <div className="team__name">
+                    <button className="remove__team" onClick={this.props.onRemoveTeam}>✖</button>
+                    {this.props.name}
+                </div>
+                <div className="team__score">
+                    <Counter onChange={this.props.onScoreChange}
+                        score={this.props.score} />
+                </div>
             </div>
-            <div className="team__score">
-                <Counter onChange={props.onScoreChange}
-                    score={props.score} />
-            </div>
-        </div>
-    )
+        )
+    }
+    
 }
 
 export default TeamSB
